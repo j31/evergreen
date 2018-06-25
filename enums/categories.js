bca576493a13da114462b83c6c2364b438b16050
John Fish <j31@users.noreply.github.com>
2018-06-25 19:17:24 +0200

    fixed jquery load order bug - ready for home reorg

---
+++ enums/categories.js
@@ -0,0 +1,25 @@
+[
+    { category: "Proverbs",             catStrength: 7,    imgFileName: "cat_proverbs.jpg"     },
+    { category: "Mythology",            catStrength: 17,   imgFileName: "cat_mythology.jpg"   },
+    { category: "Philosophy",           catStrength: 34,   imgFileName: "cat_philosophy.jpg"   },
+    { category: "Literature",           catStrength: 26,   imgFileName: "cat_literature.jpg"   },
+    { category: "Religion",             catStrength: 44,   imgFileName: "cat_religion.jpg"     },
+
+    { category: "Geography",            catStrength: 12,   imgFileName: "cat_geography.jpg"    },
+    { category: "Politics",             catStrength: 67,   imgFileName: "cat_politics.jpg"     },
+    { category: "Modern History",       catStrength: 24,   imgFileName: "cat_mod_history.jpg"  },
+    { category: "Cont. History",        catStrength: 76,   imgFileName: "cat_cont_history.jpg" },
+    { category: "Pre-modern History",   catStrength: 23,   imgFileName: "cat_pre_mod_history.jpg" },
+
+    { category: "Technology",           catStrength: 16,   imgFileName: "cat_technology.jpg"   },
+    { category: "Life Sciences",        catStrength: 29,   imgFileName: "cat_life_science.jpg"    },
+    { category: "Medicine and Health",  catStrength: 65,   imgFileName: "cat_medicine.png"        },
+    { category: "Business & Economics", catStrength: 56,   imgFileName: "cat_business.jpg"     },
+    { category: "Physical Sciences",    catStrength: 35,   imgFileName: "cat_phys_science.jpg"    },
+    { category: "Social Sciences",      catStrength: 42,   imgFileName: "cat_social_science.jpg"  },
+    { category: "Earth Sciences",       catStrength: 23,   imgFileName: "cat_earth_science.jpg"   },
+
+    { category: "Fine Arts",            catStrength: 27,   imgFileName: "cat_fine_arts.jpg"    },
+    { category: "Classical Music",      catStrength: 55,   imgFileName: "cat_clas_music.jpg"   }
+  ]
+  
