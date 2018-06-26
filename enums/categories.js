e3ceb34c009c5d876971b98e19dd9eba29514828
John Fish <j31@users.noreply.github.com>
2018-06-26 13:27:12 +0200

    added dynamic multiple categories

--- enums/categories.js
+++ enums/categories.js
@@ -1,25 +1,25 @@
 [
-    { _id: 0, group: "Writings", category: "Proverbs",             catStrength: 7,    imgFileName: "cat_proverbs.jpg"     },
-    { _id: 1, group: "Writings", category: "Mythology",            catStrength: 17,   imgFileName: "cat_mythology.jpg"   },
-    { _id: 2, group: "Writings", category: "Philosophy",           catStrength: 34,   imgFileName: "cat_philosophy.jpg"   },
-    { _id: 3, group: "Writings", category: "Literature",           catStrength: 26,   imgFileName: "cat_literature.jpg"   },
-    { _id: 4, group: "Writings", category: "Religion",             catStrength: 44,   imgFileName: "cat_religion.jpg"     },
+    { _id: 0, group: "Writings", category: "Sayings", imgFileName: "cat_proverbs.jpg"     },
+    { _id: 1, group: "Writings", category: "Mythology", imgFileName: "cat_mythology.jpg"   },
+    { _id: 2, group: "Writings", category: "Philosophy", imgFileName: "cat_philosophy.jpg"   },
+    { _id: 3, group: "Writings", category: "Literature", imgFileName: "cat_literature.jpg"   },
+    { _id: 4, group: "Writings", category: "Religion", imgFileName: "cat_religion.jpg"     },
 
-    { _id: 5, group: "World", category: "Geography",            catStrength: 12,   imgFileName: "cat_geography.jpg"    },
-    { _id: 6, group: "World", category: "Politics",             catStrength: 67,   imgFileName: "cat_politics.jpg"     },
-    { _id: 7, group: "World", category: "Modern History",       catStrength: 24,   imgFileName: "cat_mod_history.jpg"  },
-    { _id: 8, group: "World", category: "Cont. History",        catStrength: 76,   imgFileName: "cat_cont_history.jpg" },
-    { _id: 9, group: "World", category: "Pre-modern History",   catStrength: 23,   imgFileName: "cat_pre_mod_history.jpg" },
+    { _id: 5, group: "World", category: "Geography", imgFileName: "cat_geography.jpg"    },
+    { _id: 6, group: "World", category: "Politics",  imgFileName: "cat_politics.jpg"     },
+    { _id: 7, group: "World", category: "Modern History", imgFileName: "cat_mod_history.jpg"  },
+    { _id: 8, group: "World", category: "Cont. History",   imgFileName: "cat_cont_history.jpg" },
+    { _id: 9, group: "World", category: "Pre-modern History",  imgFileName: "cat_pre_mod_history.jpg" },
 
-    { _id: 10, group: "Sciences", category: "Technology",           catStrength: 16,   imgFileName: "cat_technology.jpg"   },
-    { _id: 11, group: "Sciences", category: "Life Sciences",        catStrength: 29,   imgFileName: "cat_life_science.jpg"    },
-    { _id: 12, group: "Sciences", category: "Medicine and Health",  catStrength: 65,   imgFileName: "cat_medicine.png"        },
-    { _id: 13, group: "Sciences", category: "Business & Economics", catStrength: 56,   imgFileName: "cat_business.jpg"     },
-    { _id: 14, group: "Sciences", category: "Physical Sciences",    catStrength: 35,   imgFileName: "cat_phys_science.jpg"    },
-    { _id: 15, group: "Sciences", category: "Social Sciences",      catStrength: 42,   imgFileName: "cat_social_science.jpg"  },
-    { _id: 16, group: "Sciences", category: "Earth Sciences",       catStrength: 23,   imgFileName: "cat_earth_science.jpg"   },
+    { _id: 10, group: "Sciences", category: "Technology",  imgFileName: "cat_technology.jpg"   },
+    { _id: 11, group: "Sciences", category: "Life Sciences",  imgFileName: "cat_life_science.jpg"    },
+    { _id: 12, group: "Sciences", category: "Medicine and Health", imgFileName: "cat_medicine.png"        },
+    { _id: 13, group: "Sciences", category: "Business & Economics", imgFileName: "cat_business.jpg"     },
+    { _id: 14, group: "Sciences", category: "Physical Sciences", imgFileName: "cat_phys_science.jpg"    },
+    { _id: 15, group: "Sciences", category: "Social Sciences", imgFileName: "cat_social_science.jpg"  },
+    { _id: 16, group: "Sciences", category: "Earth Sciences", imgFileName: "cat_earth_science.jpg"   },
 
-    { _id: 17, group: "Arts", category: "Fine Arts",            catStrength: 27,   imgFileName: "cat_fine_arts.jpg"    },
-    { _id: 18, group: "Arts", category: "Classical Music",      catStrength: 55,   imgFileName: "cat_clas_music.jpg"   }
+    { _id: 17, group: "Arts", category: "Fine Arts",  imgFileName: "cat_fine_arts.jpg"    },
+    { _id: 18, group: "Arts", category: "Classical Music", imgFileName: "cat_clas_music.jpg"   }
   ]
   
--- public/js/practice.js
+++ public/js/practice.js
@@ -18,29 +18,30 @@ $(document).ready(function() {
   
   // Seed data for categories
   var categoriesEnum = [
-    { _id: 0, group: "Writings", category: "Sayings",             catStrength: 7,    imgFileName: "cat_proverbs.jpg"     },
-    { _id: 1, group: "Writings", category: "Mythology",            catStrength: 17,   imgFileName: "cat_mythology.jpg"   },
-    { _id: 2, group: "Writings", category: "Philosophy",           catStrength: 34,   imgFileName: "cat_philosophy.jpg"   },
-    { _id: 3, group: "Writings", category: "Literature",           catStrength: 26,   imgFileName: "cat_literature.jpg"   },
-    { _id: 4, group: "Writings", category: "Religion",             catStrength: 44,   imgFileName: "cat_religion.jpg"     },
+    { _id: 0, group: "Writings", category: "Sayings", imgFileName: "cat_proverbs.jpg"     },
+    { _id: 1, group: "Writings", category: "Mythology", imgFileName: "cat_mythology.jpg"   },
+    { _id: 2, group: "Writings", category: "Philosophy", imgFileName: "cat_philosophy.jpg"   },
+    { _id: 3, group: "Writings", category: "Literature", imgFileName: "cat_literature.jpg"   },
+    { _id: 4, group: "Writings", category: "Religion", imgFileName: "cat_religion.jpg"     },
 
-    { _id: 5, group: "World", category: "Geography",            catStrength: 12,   imgFileName: "cat_geography.jpg"    },
-    { _id: 6, group: "World", category: "Politics",             catStrength: 67,   imgFileName: "cat_politics.jpg"     },
-    { _id: 7, group: "World", category: "Modern History",       catStrength: 24,   imgFileName: "cat_mod_history.jpg"  },
-    { _id: 8, group: "World", category: "Cont. History",        catStrength: 76,   imgFileName: "cat_cont_history.jpg" },
-    { _id: 9, group: "World", category: "Pre-modern History",   catStrength: 23,   imgFileName: "cat_pre_mod_history.jpg" },
+    { _id: 5, group: "World", category: "Geography", imgFileName: "cat_geography.jpg"    },
+    { _id: 6, group: "World", category: "Politics",  imgFileName: "cat_politics.jpg"     },
+    { _id: 7, group: "World", category: "Modern History", imgFileName: "cat_mod_history.jpg"  },
+    { _id: 8, group: "World", category: "Cont. History",   imgFileName: "cat_cont_history.jpg" },
+    { _id: 9, group: "World", category: "Pre-modern History",  imgFileName: "cat_pre_mod_history.jpg" },
 
-    { _id: 10, group: "Sciences", category: "Technology",           catStrength: 16,   imgFileName: "cat_technology.jpg"   },
-    { _id: 11, group: "Sciences", category: "Life Sciences",        catStrength: 29,   imgFileName: "cat_life_science.jpg"    },
-    { _id: 12, group: "Sciences", category: "Medicine and Health",  catStrength: 65,   imgFileName: "cat_medicine.png"        },
-    { _id: 13, group: "Sciences", category: "Business & Economics", catStrength: 56,   imgFileName: "cat_business.jpg"     },
-    { _id: 14, group: "Sciences", category: "Physical Sciences",    catStrength: 35,   imgFileName: "cat_phys_science.jpg"    },
-    { _id: 15, group: "Sciences", category: "Social Sciences",      catStrength: 42,   imgFileName: "cat_social_science.jpg"  },
-    { _id: 16, group: "Sciences", category: "Earth Sciences",       catStrength: 23,   imgFileName: "cat_earth_science.jpg"   },
+    { _id: 10, group: "Sciences", category: "Technology",  imgFileName: "cat_technology.jpg"   },
+    { _id: 11, group: "Sciences", category: "Life Sciences",  imgFileName: "cat_life_science.jpg"    },
+    { _id: 12, group: "Sciences", category: "Medicine and Health", imgFileName: "cat_medicine.png"        },
+    { _id: 13, group: "Sciences", category: "Business & Economics", imgFileName: "cat_business.jpg"     },
+    { _id: 14, group: "Sciences", category: "Physical Sciences", imgFileName: "cat_phys_science.jpg"    },
+    { _id: 15, group: "Sciences", category: "Social Sciences", imgFileName: "cat_social_science.jpg"  },
+    { _id: 16, group: "Sciences", category: "Earth Sciences", imgFileName: "cat_earth_science.jpg"   },
 
-    { _id: 17, group: "Arts", category: "Fine Arts",            catStrength: 27,   imgFileName: "cat_fine_arts.jpg"    },
-    { _id: 18, group: "Arts", category: "Classical Music",      catStrength: 55,   imgFileName: "cat_clas_music.jpg"   }
+    { _id: 17, group: "Arts", category: "Fine Arts",  imgFileName: "cat_fine_arts.jpg"    },
+    { _id: 18, group: "Arts", category: "Classical Music", imgFileName: "cat_clas_music.jpg"   }
   ]
+  
   
   
   
@@ -81,16 +82,24 @@ $(document).ready(function() {
 
 
 
+  // Find unique categories in dictionary
+    var flags = [], categoriesInDictionary = [], l = ev.dict.length, i;
+    for( i=0; i<l; i++) {
+        if( flags[ev.dict[i].cat]) continue;
+        flags[ev.dict[i].cat] = true;
+        var cat = categoriesEnum.find( cat => cat.category === ev.dict[i].cat )
+        categoriesInDictionary.push(cat);
+    }
 
-    // INJECT CATEGORY CARDS ONTO HOME PAGE
+    // INJECT CATEGORY CARDS ONTO HOME PAGE (when they have terms in dictinary)
     var html = '<div class="row justify-content-center">'
-    for (i in categoriesEnum) {
-      html += '<div class="col-xs-6"><div class="card cat-card" style="width: 9rem;"><img class="card-img-top" src="../img/categories/' + categoriesEnum[i].imgFileName + '" alt="Card image cap"><div class="card-body"><h6 class="card-title">' + categoriesEnum[i].category + '</h6><p class="card-text"><strong><span id="#">' + categoriesEnum[i].catStrength + '</span></strong> %</p></div></div></div>'
+    for (i in categoriesInDictionary) {
+      html += '<div class="col-xs-6"><div class="card cat-card" style="width: 9rem;"><img class="card-img-top" src="../img/categories/' + categoriesInDictionary[i].imgFileName + '" alt="Card image cap"><div class="card-body"><h6 class="card-title">' + categoriesInDictionary[i].category + '</h6><button type="button" class="btn btn-link cat-card-link" id="'+categoriesInDictionary[i]._id+'">Practice</button></div></div></div>'
     }
     html += '</div>'
     $('#cat-cards').html(html)
 
-  
+    
   
   //  Listen on home-menu buttons (Flashcards...)
   Evergreen.prototype.homeMenu = function () {
@@ -288,6 +297,11 @@ $(document).ready(function() {
 
         // current category score 
 
+        // update Current Category  in DOM
+        $('#current-category').text(ev.user.currentCategory.category);
+
+        console.log("CURRENT CATEGORY ", ev.user.currentCategory)
+
         // get strength scores for current category
         var categoryStrengths = ev.user.knowledge.filter( t => t.cat === ev.user.currentCategory.category)
 
@@ -332,6 +346,15 @@ $(document).ready(function() {
     ev.updateScore();
   });
   
+  $(".cat-card-link").click(function() {
+    var catId = this.id
+    var newCat = categoriesEnum.find( cat => cat._id == catId )
+    ev.user.currentCategory = newCat
+    console.log("NEW CARD PICKED", newCat);
+    ev.updateScore();
+  });
+  
+
   
   
   // Get question 
@@ -481,7 +504,7 @@ $(document).ready(function() {
           console.log("Terms in cat ", termsInCategory)
           var randomId = ev.knowledgeTerm._id
           while (randomId === ev.knowledgeTerm._id ) {
-            randomIndex = Math.floor(Math.random() * termsInCategory.length)+1
+            randomIndex = Math.floor(Math.random() * termsInCategory.length)
             randomId = termsInCategory[randomIndex]._id
           }
           
@@ -785,9 +808,7 @@ $(document).ready(function() {
       // IF SELECTION is WRONG
       if (!isCorrect) {
         
-        // PLAY "wrong" Sound
-        document.getElementById("wrong").play()
-        
+      
         
         if (ev.knowledgeTerm.strength === 0)
         ev.knowledgeTerm.strength = 1;
@@ -800,9 +821,7 @@ $(document).ready(function() {
       // IF SELECTION is CORRECT
       if (isCorrect) {
         
-        // PLAY "correct" Sound
-        document.getElementById("correct").play()
-        
+     
         // Increment Game Score
         ev.gameScore++;
         
